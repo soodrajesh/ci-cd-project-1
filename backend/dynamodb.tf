b9ceca5dbb6d87af43618947d15285ff854d8e8e
soodrajesh <rajeshsood87@gmail.com>
2023-12-17 10:18:15 +0000

    updating jenkinsfile

--- backend/dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = var.dynamodb_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
